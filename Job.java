7d965d8751ee16ec0c524273fcdca41240deb3c6
LostDesire <kjswon12@naver.com>
2020-12-15 22:10:04 +0900

    day 04 Person, Job class v1

---
+++ Job.java
@@ -0,0 +1,18 @@
+
+public class Job {
+	class Citizen extends Person{
+		
+	}
+	
+	class Scientist extends Person{
+		
+	}
+	
+	class Skrull extends Person{
+		
+	}
+	
+	class Betrayer extends Person{
+		
+	}
+}
